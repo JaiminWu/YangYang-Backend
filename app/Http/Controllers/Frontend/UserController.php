bbcc4a68d47d1063887db2c85d2be4842b8b0e85
JaiminWu <jaiminwu@163.com>
2017-11-27 19:56:47 +0800

    dev

--- app/Http/Controllers/Frontend/UserController.php
+++ app/Http/Controllers/Frontend/UserController.php
@@ -54,12 +54,15 @@ class UserController extends Controller
     }
 
     public function checkLogin(Request $request){
-        $user_id = $request->session()->get('user_id', null);
-        if(empty($user_id)){
-            $this->errorHandler(303);
-        }
-        $this->errorHandler(1, $user_id);
+//        $user_id = $request->session()->get('user_id', null);
+//        if(empty($user_id)){
+//            $this->errorHandler(303);
+//        }
+//        $this->errorHandler(1, $user_id);
+        $this->errorHandler(1);
+
     }
+
 
     public function show(Request $request){
 
